306184c74b4a28ea83da1f3f927c0da442c07700
KoredeDavid <koredeoluwashola@gmail.com>
2022-06-15 13:47:26 +0100

    First Commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+window.addEventListener("DOMContentLoaded", async function (event) {
+
+    const navBarTogglers = document.querySelectorAll('.toggle-navbar')
+    const navBarModal = document.querySelector('#navbarModal')
+
+    for (const toggler of navBarTogglers) {
+        toggler.addEventListener('click', async function (e) {
+            e.preventDefault()
+
+            const scrollTo = e.currentTarget.getAttribute('data-target')
+
+            if (window.innerWidth < 992) {
+                if (window.innerWidth < 992) {
+                    navBarModal.classList.toggle('show')
+                }
+
+                // Sleeps for 1s before continuing
+                await new Promise(resolve => setTimeout(resolve, 500))
+
+                if (scrollTo) {
+                    document.querySelector(`.${scrollTo}`).scrollIntoView({behavior:"smooth"})
+                }
+            }
+            else{
+                if (scrollTo) {
+                    document.querySelector(`.${scrollTo}`).scrollIntoView({behavior:"smooth"})
+                }
+            }
+        })
+
+    }
+})
+
+
+
